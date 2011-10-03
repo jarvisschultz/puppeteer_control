d240ef73f04b05b7b3958850e12fcdfb2ed1813d
Jarvis Schultz <jschultz@u.northwestern.edu>
2011-10-03 13:34:14 -0700

    Modified this code to run off of its own timer.  Thus it does not depend on the estimator node anymore.

--- src/position_control.cpp
+++ src/position_control.cpp
@@ -73,18 +73,14 @@ private:
     Trajectory *traj;
     ros::NodeHandle n_;
     ros::ServiceClient client;
-    ros::Subscriber sub;
+    // ros::Subscriber sub;
+    ros::Timer timer;
     puppeteer_msgs::speed_command srv;
     puppeteer_msgs::RobotPose pose;
     bool start_flag;
-    float desired_x, desired_y, desired_th, actual_x, actual_y, actual_th;
-    float vd, wd;
+    float desired_x, desired_y, desired_th;
     unsigned int num;
-    // Controller gains
-    float k1, k2, k3;
-    float zeta, b;
-    std::ofstream tmp_file;
-
+    
 public:
     KinematicControl() {
 	// Initialize necessary variables:
@@ -103,34 +99,35 @@ public:
 	// Define service client:
 	client = n_.serviceClient<puppeteer_msgs::speed_command>("speed_command");
 	// Define subscriber:
-	sub = n_.subscribe("/robot_pose", 1, &KinematicControl::subscriber_cb, this);
+	// sub = n_.subscribe("/robot_pose", 1, &KinematicControl::subscriber_cb, this);
+	// Define timer:
+	timer = n_.createTimer(ros::Duration(0.033), &KinematicControl::subscriber_cb, this);
 
 	// Send a start flag:
 	send_start_flag();
 	
-	// set control gain values:
-	zeta = 0.7;
-	b = 10;
-
-	tmp_file.open("temp.txt");
     }
 
     // This gets called every time the estimator publishes a new robot
     // pose
-    void subscriber_cb(const puppeteer_msgs::RobotPose &pose)
+    void subscriber_cb(const ros::TimerEvent& e)
 	{
 	    static double running_time = 0.0;
 	    static ros::Time base_time;
 	    ros::param::get("/operating_condition", operating_condition);
+
+	    ROS_DEBUG("Pose callback triggered");
 	    
 	    if (operating_condition == 0 || operating_condition == 1 ||
 		operating_condition == 3)
 	    {
+		ROS_DEBUG("A non-runnning condition");
 		start_flag = true;
 		return;
 	    }
 	    else if (operating_condition == 2)
 	    {
+		ROS_DEBUG("Running Condition");
 		if (start_flag == true)
 		{
 		    ROS_INFO("Beginning movement execution");
@@ -150,6 +147,7 @@ public:
 		}
 		else
 		{
+		    ROS_DEBUG("Getting values to send");
 		    // we will run the regular control loop
 		    // let's first get the expected pose at the given time:
 		    running_time = ((ros::Time::now()).toSec()-
@@ -157,8 +155,8 @@ public:
 		    // check that running_time is less than the final time:
 		    if (running_time <= traj->vals[num-1][0])
 		    {
+			ROS_DEBUG("Getting desired pose and control values");
 			get_desired_pose(running_time);
-			get_control_values(pose, running_time);
 		    }
 		    else
 		    {
@@ -178,7 +176,7 @@ public:
 	    }
 	    else if (operating_condition == 4)
 	    {
-		ROS_WARN("Emergency Stop Detected!");
+		ROS_WARN_THROTTLE(5,"Emergency Stop Detected!");
 		srv.request.robot_index = traj->RobotMY;
 		srv.request.type = 'h';
 		srv.request.Vleft = 0.0;
@@ -188,30 +186,25 @@ public:
 		start_flag = true;
 	    }
 
-	    static int i = 0;
-	    if (i < 4)
+	    ROS_DEBUG("Calling service");
+	    // send request to service
+	    if(client.call(srv))
 	    {
-		// send request to service
-		if(client.call(srv))
+		if(srv.response.error == false)
+		    ROS_DEBUG("Send Successful: speed_command\n");
+		else
 		{
-		    if(srv.response.error == false)
-			ROS_DEBUG("Send Successful: speed_command\n");
-		    else
+		    ROS_DEBUG("Send Request Denied: speed_command\n");
+		    static bool request_denied_notify = true;
+		    if(request_denied_notify)
 		    {
-			ROS_DEBUG("Send Request Denied: speed_command\n");
-			static bool request_denied_notify = true;
-			if(request_denied_notify)
-			{
-			    ROS_INFO("Send Requests Denied: speed_command\n");
-			    request_denied_notify = false;
-			}
+			ROS_INFO("Send Requests Denied: speed_command\n");
+			request_denied_notify = false;
 		    }
 		}
-		else 
-		    ROS_ERROR("Failed to call service: speed_command\n");
 	    }
-	    else
-		assert(0);
+	    else 
+		ROS_ERROR("Failed to call service: speed_command\n");
 	}
 
     void get_desired_pose(float time)
@@ -244,100 +237,17 @@ public:
 	    while (desired_th > M_PI)
 		desired_th -= 2.0*M_PI;
 
-	    // Now, we can interpolate the feedforward terms:
-	    vd = (traj->vals[index-1][3])+
-		mult*(traj->vals[index][3]-traj->vals[index-1][3]);
-	    wd = (traj->vals[index-1][4])+
-		mult*(traj->vals[index][4]-traj->vals[index-1][4]);
-
-	    tmp_file << time << ",";
-	    tmp_file << desired_x << ",";
-	    tmp_file << desired_y << ",";
-	    tmp_file << desired_th << ",";
-	    tmp_file << vd << ",";
-	    tmp_file << wd << ",";
-		
+	    ROS_INFO("Time = %f", time);
 	    ROS_INFO("Xd = %f\tYd = %f\tTd = %f\t",desired_x, desired_y, desired_th);
-	    return;
-	}
 
-    void get_control_values(const puppeteer_msgs::RobotPose &pose, const double time)
-	{
-	    float v, omega, vleft, vright, dtheta;
-	    float comps[3];
-	    // This function takes no arguments, it just calculates
-	    // the wheel velocities we should send as dictated by the
-	    // closed loop controller.  It also sets those parameters
-	    // in the serial service
-
-	    // find the robot returned values:
-	    actual_x = pose.x_robot;
-	    actual_y = pose.y_robot;
-	    actual_th = pose.theta;
-	    ROS_INFO("Xa = %f\tYa = %f\tTa = %f\t",actual_x, actual_y, actual_th);
-
-	    // Now calculate the gain values:
-	    k1 = 2*zeta*sqrt(pow(wd,2)+b*pow(vd,2));
-	    k2 = b*fabs(vd);
-	    k3 = k1;
-
-	    tmp_file << k1 << ",";
-	    tmp_file << k2 << ",";	    
-
-	    // calc control values:
-	    v = vd*cos(desired_th-actual_th) +
-		k1*(cos(actual_th)*(desired_x-actual_x)+
-		    sin(actual_th)*(desired_y-actual_y));
-	    comps[0] = desired_th-actual_th-2.0*M_PI;
-	    comps[1] = desired_th-actual_th+2.0*M_PI;
-	    comps[2] = desired_th-actual_th;
-	    if (abs(comps[0]) < abs(comps[1]))
-	    {
-		if (abs(comps[0]) < abs(comps[2]))
-		    dtheta = comps[0];
-		else
-		    dtheta = comps[2];
-	    }
-	    else
-	    {
-		if (abs(comps[1]) < abs(comps[2]))
-		    dtheta = comps[1];
-		else
-		    dtheta = comps[2];
-	    }
-	    omega = wd + k2*((float) sgn(vd))*
-		(cos(actual_th)*(desired_y-actual_y)-
-		 sin(actual_th)*(desired_x-actual_x))
-		+ k3*dtheta;
-
-	    // Now we can convert those to angular wheel velocities:
-	    // vright = (v+omega*WIDTH)/DWHEEL/2.0;
-	    // vleft = (v-omega*WIDTH)/DWHEEL/2.0;
-	    vright = (2.0*v+omega*WIDTH)/DWHEEL;
-	    vleft = (2.0*v-omega*WIDTH)/DWHEEL;
-	    
-	    tmp_file << vright << ",";
-	    tmp_file << vleft << "\n";	    
-	    
-	    while (vright > MAX_ANG_VEL || vleft > MAX_ANG_VEL)
-	    {
-		vright *= 0.9;
-	        vleft *= 0.9; 
-	    }
-
-	    ROS_INFO("Vleft = %f\tVright = %f",vleft,vright);
 	    // Set service parameters:
 	    srv.request.robot_index = traj->RobotMY;
-	    // srv.request.type = 'h';
-	    // srv.request.Vleft = vleft;
-	    // srv.request.Vright = vright;
-	    // srv.request.Vtop = 0.0;
-	    // srv.request.div = 3;
 	    srv.request.type = 'k';
 	    srv.request.Vleft = time;
 	    srv.request.Vright = desired_x;
 	    srv.request.Vtop = desired_y;
 	    srv.request.div = 4;
+	    
 	    return;
 	}
 
@@ -518,7 +428,7 @@ void command_line_parser(int argc, char** argv)
 int main(int argc, char** argv)
 {
     // startup node
-    ros::init(argc, argv, "kinematic_controller");
+    ros::init(argc, argv, "position_controller");
     ros::NodeHandle n;
 
     command_line_parser(argc, argv);
